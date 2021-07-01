da290f8a69455466aac2c886c68380cab3957ef4
shihaji <shihajig@gmail.com>
2021-07-01 16:40:49 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeleteComponent } from './delete/delete.component';
+import { RegisterComponent } from './register/register.component';
+import { UpateComponent } from './upate/upate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"register",component:RegisterComponent},
+  {path:"update",component:UpateComponent},
+  {path:"delete" ,component:DeleteComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
   title = 'my-app';
+
+  flag:boolean=true;
+
+  hideLogin(status:boolean){
+     this.flag=status;
+  }
+
+
 }
